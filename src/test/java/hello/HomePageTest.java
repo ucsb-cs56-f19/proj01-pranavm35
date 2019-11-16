af09bb08275d7709a7fc30a4525376d4d8629c8a
Pranav Madapurmath <pranavmadapurmath@Pranavs-MacBook-Pro.local>
2019-11-16 13:36:32 -0800

    junit Earthquakes - has correct link

--- src/test/java/hello/HomePageTest.java
+++ src/test/java/hello/HomePageTest.java
@@ -69,4 +69,12 @@ public class HomePageTest {
                 .andExpect(xpath("/html/body/div/nav/a").exists())
                 .andExpect(xpath("/html/body/div/nav/a").string("lab07"));
     }
+
+    @Test
+    public void getHomePage_hasCorrectLink() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.TEXT_HTML))
+                .andExpect(status().isOk())
+                .andExpect(xpath("/html/body/div/nav/a").exists())
+                .andExpect(xpath("//*[@id='navbarTogglerDemo03']/ul[1]/li[2]/a").string("Earthquakes"));
+    }
 }
