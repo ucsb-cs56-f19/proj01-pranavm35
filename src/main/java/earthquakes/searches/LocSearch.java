e0cc818d383a20b8efd8e7a536a99c2f926fbdc7
Pranav Madapurmath <pranavmadapurmath@Pranavs-MBP.attlocal.net>
2019-11-30 15:42:47 -0800

    pm - set up location search form and linked it to results page

---
+++ src/main/java/earthquakes/searches/LocSearch.java
@@ -0,0 +1,13 @@
+package earthquakes.searches;
+
+public class LocSearch {
+    private String location;
+
+    public LocSearch() {
+
+    }
+
+    public String getLocation() { return location; }
+
+    public void setLocation(String loc) { location = loc; }
+}
