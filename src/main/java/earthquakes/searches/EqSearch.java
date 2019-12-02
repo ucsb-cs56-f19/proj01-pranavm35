7d125c33a6c170223ce36ab297520008b5b26ae9
Pranav Madapurmath <pranavmadapurmath@Pranavs-MacBook-Pro.local>
2019-12-01 19:36:10 -0800

    pm - added lat, lon, location fields to earthquake search/results; still need to accept user input and display appropriate results

--- src/main/java/earthquakes/searches/EqSearch.java
+++ src/main/java/earthquakes/searches/EqSearch.java
@@ -3,6 +3,8 @@ package earthquakes.searches;
 public class EqSearch {
     private int distance;
     private int minmag;
+    private double lat, lon;
+    private String location;
 
     public EqSearch(){
 
@@ -10,7 +12,14 @@ public class EqSearch {
 
     public int getDistance() { return distance; }
     public int getMinmag() { return minmag; }
+    public double getLat() { return lat; }
+    public double getLon() { return lon; }
+    public String getLocation() { return location; }
 
     public void setDistance(int dist) { distance = dist; }
     public void setMinmag(int mm) { minmag = mm; }
+    public void setLat(double la) { lat = la; }
+    public void setLon(double lo) { lon = lo; }
+    public void setLocation(String loc) { location = loc; }
+
 }
